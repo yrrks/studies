3130de122b8e0dceb6272305b9f92e0508756329
yrkks <yryryrkks95@mail.ru>
2022-09-14 12:54:57 +0300

    небольшие изменения

--- homeworks/lesson7/Ex03/Program.cs
+++ homeworks/lesson7/Ex03/Program.cs
@@ -1,18 +1,18 @@
 ﻿float[,] Array2D = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
 void AverageColumn(float[,] Array)
 {
-    int sizeColumn = Array.GetLength(1);
-    int sizeLine = Array.GetLength(0);
-    for (int i = 0; i < sizeColumn; i++)
+    int sizeLine = Array.GetLength(1);
+    int sizeColumn = Array.GetLength(0);
+    for (int i = 0; i < sizeLine; i++)
     {
         float averageCol = 0;
         float summ = 0;
 
-        for (int j = 0; j < sizeLine; j++)
+        for (int j = 0; j < sizeColumn; j++)
         {
             summ = summ + Array[j, i];
         }
-        averageCol = summ / sizeLine;
+        averageCol = summ / sizeColumn;
         Console.WriteLine($"Среднее арифметическое {i + 1} столбца = {averageCol}");
     }
 }
