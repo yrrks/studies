4ae60b12d345c111a9d64a976186255d7d73a46b
yrkks <yryryrkks95@mail.ru>
2022-09-15 14:05:58 +0300

    небольшие правки

--- homeworks/lesson8/Ex02/Program.cs
+++ homeworks/lesson8/Ex02/Program.cs
@@ -1,20 +1,27 @@
-﻿int[,,] Array3D = { { { 66, 27 }, { 25, 90 } }, { { 34, 26 }, { 41, 55 } } };
-void ShowArray(int[,,] array)
+﻿int[,] Array2D = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 }, { 5, 2, 6, 7 } };
+void FindMinLine(int[,] array)
 {
-    int arrayX = array.GetLength(0);
-    int arrayY = array.GetLength(1);
-    int arrayZ = array.GetLength(2);
-    for (int i = 0; i < arrayZ; i++)
+    int sizeLine = array.GetLength(1);
+    int sizeColumn = array.GetLength(0);
+    int minLine = 0;
+    int numLine = 1;
+    for (int i = 0; i < sizeLine; i++)
     {
-        for (int j = 0; j < arrayY; j++)
+        minLine = minLine + array[0, i];
+    }
+    for (int j = 0; j < sizeColumn; j++)
+    {
+        int comparison = 0;
+        for (int m = 0; m < sizeLine; m++)
         {
-            Console.WriteLine(" ");
-            for (int m = 0; m < arrayX; m++)
-            {
-                int number = array[j, m, i];
-                Console.Write($"{number}({j},{m},{i}) ");
-            }
+            comparison = comparison + array[j, m];
+        }
+        if (comparison < minLine)
+        {
+            minLine = comparison;
+
         }
     }
+    Console.WriteLine($"{numLine} строка минимальная со значением {minLine}");
 }
-ShowArray(Array3D);
+FindMinLine(Array2D);
---
+++ homeworks/lesson8/Ex03/Program.cs
@@ -0,0 +1,20 @@
+﻿int[,,] Array3D = { { { 66, 27 }, { 25, 90 } }, { { 34, 26 }, { 41, 55 } } };
+void ShowArray(int[,,] array)
+{
+    int arrayX = array.GetLength(0);
+    int arrayY = array.GetLength(1);
+    int arrayZ = array.GetLength(2);
+    for (int i = 0; i < arrayZ; i++)
+    {
+        for (int j = 0; j < arrayY; j++)
+        {
+            Console.WriteLine(" ");
+            for (int m = 0; m < arrayX; m++)
+            {
+                int number = array[j, m, i];
+                Console.Write($"{number}({j},{m},{i}) ");
+            }
+        }
+    }
+}
+ShowArray(Array3D);
